9e95d175f93ca4a8504af394bbc89a9be8d54f59
Shubhbansal44 <shubh.bansal1504@gmail.com>
2024-12-14 14:24:02 +0530

    fixed typescript error(user module agumentation)

--- auth/src/auth.ts
+++ auth/src/auth.ts
@@ -3,6 +3,7 @@ import authConfig from "@/auth.config"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { db } from "@/lib/db"
 import { getUserById } from "@/utils/users.utils"
+import { UserRole } from "@prisma/client"
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
@@ -18,13 +19,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return token;
     },
     async session({ session, token }) {
-      console.log("session token: ", token)
       if(token.sub && session.user) {
         session.user.id = token.sub;
       }
       
       if(token.role && session.user) {
-        session.user.role = token.role;
+        session.user.role = token.role as UserRole;
       }
 
       return session;
--- auth/src/next-auth.d.ts
+++ auth/src/next-auth.d.ts
@@ -1,20 +1,12 @@
-// // types/next-auth.d.ts
-// import NextAuth, { DefaultSession } from "next-auth";
+import { UserRole } from "@prisma/client";
+import NextAuth, { DefaultSession } from "next-auth";
 
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id: string;
-//       email: string;
-//       name: string;
-//       role: string;  // Add your custom properties here
-//     } & DefaultSession["user"];
-//   }
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole
+}
 
-//   interface User {
-//     id: string;
-//     email: string;
-//     name: string;
-//     role: string;  // Add your custom properties here
-//   }
-// }
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
